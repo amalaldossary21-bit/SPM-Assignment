feb5f0bc3ce1e2e0128a7f51a3778ef11f505c59
amalaldossary21-bit <amalaldossary21@gmail.com>
2026-05-09 21:00:54 +0300

    Add findMax and modulus methods

--- Operations.java
+++ Operations.java
@@ -1,1 +1,23 @@
+public class Operations {
 
+    // Find the maximum of two numbers
+    public static int findMax(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    // Calculate modulus (remainder)
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        int x = 10, y = 3;
+        System.out.println("Maximum: " + findMax(x, y));
+        System.out.println("Modulus (remainder): " + modulus(x, y));
+    }
+}
