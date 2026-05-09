dea87573fd889f2a2c6a0bccb5b868c62251ae15
shaikhah-pmu <202200256@pmu.edu.sa>
2026-05-09 23:05:46 +0300

    Added multiply and divide methods with output screenshot

--- Operations.java
+++ Operations.java
@@ -1,1 +1,17 @@
+public class Operations {
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Multiply: " + multiply(4,5));
+        System.out.println("Divide: " + divide(20,5));
+
+    }
+}
