18a99dff30c537e42741ccffda613a3f5fb17288
razanns <202200045@pmu.edu.sa>
2026-05-09 23:27:24 +0300

    Added even and odd methods

--- Operations.java
+++ Operations.java
@@ -1,1 +1,18 @@
+public class Operations {
 
+    public static boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    public static boolean isOdd(int a) {
+        return a % 2 != 0;
+    }
+
+    public static void main(String[] args) {
+
+        int x = 7;
+
+        System.out.println("Is Even: " + isEven(x));
+        System.out.println("Is Odd: " + isOdd(x));
+    }
+}
