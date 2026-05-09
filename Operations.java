7dd1e850ba4a10c9852e4dd7c93c3ec23d6b5d80
MLeen3 <202101242@pmu.edu.sa>
2026-05-09 23:32:22 +0300

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -1,15 +1,17 @@
 public class Operations {
 
+    // Method 1: Subtraction
     public static int subtract(int a, int b) {
         return a - b;
     }
 
-    public static int subtractFromZero(int a) {
-        return 0 - a;
+    // Method 2: Addition
+    public static int add(int a, int b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
-        System.out.println("10 - 4 = " + subtract(10, 4));
-        System.out.println("0 - 7 = " + subtractFromZero(7));
+        System.out.println("Subtraction: 10 - 4 = " + subtract(10, 4));
+        System.out.println("Addition: 10 + 4 = " + add(10, 4));
     }
 }
