9223b9f9e95394af0ed39ce623c43be0e38be114
MLeen3 <202101242@pmu.edu.sa>
2026-05-09 22:10:08 +0300

    Added subtraction methods

--- Operations.java
+++ Operations.java
@@ -1,1 +1,15 @@
+public class Operations {
 
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int subtractFromZero(int a) {
+        return 0 - a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("10 - 4 = " + subtract(10, 4));
+        System.out.println("0 - 7 = " + subtractFromZero(7));
+    }
+}
